da11ba91666a8a5de4709a87b658da51878c53f9
Divya <divyagoud1721@gmail.com>
2020-05-18 09:58:13 -0400

    Updates

---
+++ precache-manifest.a3f4571e1e6819294c2696edd68cc2a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dff4d18dd5bd698e8cfd59cb171d24",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "d6f4a8b27714cb8696c6",
+    "url": "/react-app/static/css/main.7225289e.chunk.css"
+  },
+  {
+    "revision": "bd6fabf160324eaf811e",
+    "url": "/react-app/static/js/2.f07b39a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.f07b39a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f4a8b27714cb8696c6",
+    "url": "/react-app/static/js/main.a55a2e4c.chunk.js"
+  },
+  {
+    "revision": "2859edfa3cf6df56101c",
+    "url": "/react-app/static/js/runtime-main.b41e6653.js"
+  }
+]);
